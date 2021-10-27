de986f6326c1e0f9861637a0d031fba05aa888f8
surtich <surtich@gmail.com>
2021-10-27 12:26:04 +0200

    Added basic async samples

---
+++ js/dist/asyn.dev.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function add(x, y) {
+  return x + y;
+}
+
+function asyncAdd(x, y, cbk) {
+  setTimeout(function () {
+    cbk(x + y);
+  }, Math.floor(Math.random() * 50) + 50);
+}
+
+function asyncProduct(x, y, cbk) {
+  setTimeout(function () {
+    cbk(x * y);
+  }, Math.floor(Math.random() * 50) + 50);
+}
+
+asyncAdd(5, 6, function (result) {
+  console.log(result);
+});
+asyncProduct(6, 8, function (result) {
+  console.log(result);
+});
