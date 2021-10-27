4854093328a9a8bf921b172fa95c892adf1c2801
surtich <surtich@gmail.com>
2021-10-27 12:56:09 +0200

    Added async exercises

--- js/asyn.js
+++ js/asyn.js
@@ -1,8 +1,3 @@
-function add(x, y) {
-    return x + y;
-}
-
-
 function asyncAdd(x, y, cbk) {
     setTimeout(function () {
         cbk(x + y);
@@ -15,10 +10,45 @@ function asyncProduct(x, y, cbk) {
     }, Math.floor(Math.random() * 50) + 50);
 }
 
-asyncAdd(5, 6, function (result) {
-    console.log(result);
-});
 
 asyncProduct(6, 8, function (result) {
     console.log(result);
 });
+
+asyncProduct(3, 4, function (product) {
+    asyncAdd(product, 5, function (sum) {
+        asyncProduct(sum, 8, function (result) {
+            console.log(result);
+        });
+    });
+});
+
+const step3 = (x, y) => asyncProduct(x, y, function (result) {
+    console.log(result);
+});
+
+const step2 = (x, y, z) => asyncAdd(x, y, function (sum) {
+    step3(sum, z);
+});
+
+const step1 = (x, y, z, k) => asyncProduct(x, y, function (product) {
+    step2(product, z, k);
+});
+
+
+step1(3, 4, 5, 8);
+
+// Sumar asíncronamente [1, 4, 6, 2, 4] == 17; implementando 
+
+function asyncAddArray(xs, cbk) {
+
+}
+
+// Sumar asíncronamente [1, 2, 3, 4] + [0, -1, 1, 2] ==  [1, 1, 4, 6]; implementando
+
+function asyncAddArrays(xs, ys, cbk) {
+}
+
+
+
+
--- js/dist/asyn.dev.js
+++ js/dist/asyn.dev.js
@@ -1,9 +1,5 @@
 "use strict";
 
-function add(x, y) {
-  return x + y;
-}
-
 function asyncAdd(x, y, cbk) {
   setTimeout(function () {
     cbk(x + y);
@@ -16,9 +12,38 @@ function asyncProduct(x, y, cbk) {
   }, Math.floor(Math.random() * 50) + 50);
 }
 
-asyncAdd(5, 6, function (result) {
-  console.log(result);
-});
 asyncProduct(6, 8, function (result) {
   console.log(result);
 });
+asyncProduct(3, 4, function (product) {
+  asyncAdd(product, 5, function (sum) {
+    asyncProduct(sum, 8, function (result) {
+      console.log(result);
+    });
+  });
+});
+
+var step3 = function step3(x, y) {
+  return asyncProduct(x, y, function (result) {
+    console.log(result);
+  });
+};
+
+var step2 = function step2(x, y, z) {
+  return asyncAdd(x, y, function (sum) {
+    step3(sum, z);
+  });
+};
+
+var step1 = function step1(x, y, z, k) {
+  return asyncProduct(x, y, function (product) {
+    step2(product, z, k);
+  });
+};
+
+step1(3, 4, 5, 8); // Sumar asíncronamente [1, 4, 6, 2, 4] == 17; implementando 
+
+function asyncAddArray(xs, cbk) {} // Sumar asíncronamente [1, 2, 3, 4] + [0, -1, 1, 2] ==  [1, 1, 4, 6]; implementando
+
+
+function asyncAddArrays(xs, ys, cbk) {}
